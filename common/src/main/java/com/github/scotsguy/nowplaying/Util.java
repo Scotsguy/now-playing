a8e95d0972e01386af9973f7c95818b163b08000
AppleTheGolden <scotsbox@protonmail.com>
2022-01-09 21:14:45 +0100

    Fix #10

--- common/src/main/java/com/github/scotsguy/nowplaying/Util.java
+++ common/src/main/java/com/github/scotsguy/nowplaying/Util.java
@@ -1,23 +1,23 @@
 package com.github.scotsguy.nowplaying;
 
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.RecordItem;
 import com.github.scotsguy.nowplaying.mixin.RecordItemAccessor;
 
 public class Util {
     public static Component getSoundName(SoundInstance instance) {
-        if (instance.getSound().getLocation().toString().startsWith("minecraft:music/")) {
-            String id = "now_playing.sound." + instance.getSound().getLocation().toString();
-
-            return new TranslatableComponent(id);
+        String soundLocation = instance.getSound().getLocation().toString();
+        if (soundLocation.startsWith("minecraft:music/") || I18n.exists("now_playing.sound." + soundLocation)) {
+            return new TranslatableComponent("now_playing.sound." + soundLocation);
         }
         return null;
     }
     public static RecordItem getDiscFromSound(SoundInstance instance) {
-        //SoundEvent event = new SoundEvent(new Identifier(instance.getId().toString().replace('.', '_')));
         for (SoundEvent event : RecordItemAccessor.getDiscs().keySet()) {
             if (event.getLocation().equals(instance.getLocation())) {
                 return RecordItem.getBySound(event);
