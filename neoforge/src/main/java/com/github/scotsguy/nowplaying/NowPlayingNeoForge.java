724aaed52e0b003bfff5f52a348b8b1cf73f40ef
NotRyken <notryken@gmail.com>
2024-05-26 20:53:12 +0800

    Update to Java 21 and mc1.20.6

--- neoforge/src/main/java/com/github/scotsguy/nowplaying/NowPlayingNeoForge.java
+++ neoforge/src/main/java/com/github/scotsguy/nowplaying/NowPlayingNeoForge.java
@@ -3,15 +3,13 @@ package com.github.scotsguy.nowplaying;
 import com.github.scotsguy.nowplaying.gui.screen.ConfigScreenProvider;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(NowPlaying.MOD_ID_NEOFORGE)
 public class NowPlayingNeoForge {
     public NowPlayingNeoForge() {
-        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
-                () -> new ConfigScreenHandler.ConfigScreenFactory(
-                        (mc, parent) -> ConfigScreenProvider.getConfigScreen(parent))
-                );
+        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class,
+                () -> (mc, parent) -> ConfigScreenProvider.getConfigScreen(parent));
 
         NowPlaying.init();
     }
